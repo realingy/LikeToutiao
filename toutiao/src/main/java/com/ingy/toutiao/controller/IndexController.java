538fdf8220c8797f03f68d7a1fab8a69129de647
realingy <754037927@qq.com>
2019-08-08 15:44:55 +0800

    第二次课代码

--- toutiao/src/main/java/com/ingy/toutiao/controller/IndexController.java
+++ toutiao/src/main/java/com/ingy/toutiao/controller/IndexController.java
@@ -1,25 +1,36 @@
 package com.ingy.toutiao.controller;
 
+import com.ingy.toutiao.service.ToutiaoService;
 import org.apache.tomcat.jni.User;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 
 @Controller
+//@RestController
 public class IndexController {
+
+//    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    private ToutiaoService toutiaoService;
+
     @RequestMapping(path = {"/", "/index"})
     @ResponseBody
-    public String index() {
-        return "Hello Ingy";
+//    @GetMapping(path = {"/", "/index"})
+    public String index(HttpSession session) {
+//        return "Hello Ingy";
+        return "Hello NowCoder," + session.getAttribute("msg") + "Say: " + toutiaoService.say();
     }
 
     @RequestMapping(value = {"/profile/{groupId}/{userId}"})
@@ -28,12 +39,17 @@ public class IndexController {
                           @PathVariable("userId") int userId,
                           @RequestParam(value = "type", defaultValue = "1") int type,
                           @RequestParam(value = "key", defaultValue = "ingy") String key) {
+        //带默认值的形参
         return String.format("GID{%s},UID{%d},TYPE{%d},KEY{%s}", groupId, userId, type, key);
+//      访问方式：  http://localhost:8080/profile/123/123?key=张三&type=2
+//      访问方式：  http://localhost:8080/profile/123/123
     }
 
-    @RequestMapping(value = {"/vm"})
+//    @RequestMapping(value = {"/templates"})
+//    @RequestMapping(value = {"/vm"})
 //    public String news(Model model) {
-    public String news() {
+    @GetMapping("/vm")
+    public String news(HttpServletRequest req) {
 //        model.addAttribute("value1", "vv1");
 //        List<String> colors = Arrays.asList(new String[]{"RED", "GREEN", "BLUE"});
 //
@@ -46,7 +62,80 @@ public class IndexController {
 //        model.addAttribute("map", map);
 //        model.addAttribute("user", new User("Jim"));
 
-        return "news";
+//        return "news";
+        //逻辑处理
+        req.setAttribute("key", "hello world");
+        return "index";
+    }
+
+    @RequestMapping(value = {"/request"})
+    @ResponseBody
+    public String request(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpSession session)  {
+
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name + ":" + request.getHeader(name) + "<br>");
+        }
+
+        for (Cookie cookie : request.getCookies()) {
+            sb.append("Cookie:");
+            sb.append(cookie.getName());
+            sb.append(":");
+            sb.append(cookie.getValue());
+            sb.append("<br>");
+        }
+
+        sb.append("getMethod:" + request.getMethod() + "<br>");
+        sb.append("getPathInfo:" + request.getPathInfo() + "<br>");
+        sb.append("getQueryString:" + request.getQueryString() + "<br>");
+        sb.append("getRequestURI:" + request.getRequestURI() + "<br>");
+
+        return sb.toString();
+    }
+
+    @RequestMapping(value = {"/response"})
+    @ResponseBody
+    public String response(@CookieValue(value = "nowcoderid", defaultValue = "a") String nowcoderId,
+                           @RequestParam(value = "key", defaultValue = "key") String key,
+                           @RequestParam(value = "value", defaultValue = "value") String value,
+                           HttpServletResponse response) {
+        response.addCookie(new Cookie(key, value));
+        response.addHeader(key, value);
+        return "NowCoderId From Cookie:" + nowcoderId;
+    }
+
+    //重定向
+    @RequestMapping("/redirect/{code}")
+    public String redirect(@PathVariable("code") int code,
+                           HttpSession session) {
+        /*
+        RedirectView red = new RedirectView("/", true);
+        if (code == 301) {
+            red.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+        return red;*/
+        session.setAttribute("msg", "Jump from redirect.");
+        return "redirect:/";
+    }
+
+    //模拟异常
+    @RequestMapping("/admin")
+    @ResponseBody
+    public String admin(@RequestParam(value = "key", required = false) String key) {
+        if ("admin".equals(key)) {
+            return "hello admin";
+        }
+        throw new IllegalArgumentException("Key 错误");
+    }
+
+    @ExceptionHandler()
+    @ResponseBody
+    public String error(Exception e) {
+        return "error:" + e.getMessage();
     }
 
 }
